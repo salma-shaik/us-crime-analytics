371cde94b62778754b3af359faff0ac57e14b4a7
salma-shaik <salmashaik1611@gmail.com>
2019-09-03 12:13:21 -0400

    updated census data interpolator file to use updated interpoaltion code

---
+++ utilities/census_data_interpolator.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+from utilities import fixed_columns_replicator as fcr
+from utilities import year_replicator as yr
+from utilities import year_generator as yg
+from utilities import variables_interpolator as vi
+
+
+# replicate fixed columns into required number of times depending on year
+# 2015 - 5; 2010, 2000 - 10; 1990 - 1
+
+fcr.replicate_fixed_cols(df = pd.read_csv('/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census/Census_90-15_Final_Sorted.csv'),
+                         cols_list=['ORI', 'AGENCY', 'placename', 'Govt_level', 'place_fips', 'STATEFP', 'CNTY', 'YEAR'],
+                         op_fl_path='/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census',
+                         dt_type='Census')
+
+
+'''
+# replicate year set from 90-15 unique ORI number of times
+yr.genereate_years_90_15(df = pd.read_csv(
+                                '/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census/Census_Fixed_Cols_Replicated.csv'),
+                         repl_times = 14542,
+                         dt_type = 'Census',
+                         op_fl_path='/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census')
+'''
+
+# generate years between existing decennial years
+yg.generate_years(df=pd.read_csv(
+                                '/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census/Census_Fixed_Cols_Replicated.csv'),
+                         dt_type = 'Census',
+                         op_fl_path='/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census')
+
+
+# interpolate variable values in between decennial years
+vi.interpolate_vars(df=pd.read_csv('C:/Users/sshaik2/projects/criminal_justice/us-crime-analytics/data/economic/new_eco_cen/Census_90-15_Final_Sorted.csv'),
+                    dt_type='Census_Pop_Vars',
+                    op_fl_path='/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census')
+
+
+# Merge the interpolateed files together by ORI and YEAR
+
+''''
+############## TO-DO ####################
+do the below
+'''
+# replace_negative_values()
+# write_final_intpltd_file()
