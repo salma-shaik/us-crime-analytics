8969dde2c63fdbeb3e31b2047d47ae080f5f7881
salma-shaik <salmashaik1611@gmail.com>
2019-09-05 15:47:32 -0400

    updated cen data int to replace neg values and merge all

--- utilities/census_data_interpolator.py
+++ utilities/census_data_interpolator.py
@@ -5,6 +5,7 @@ from utilities import year_replicator as yr
 
 from utilities import year_generator as yg
 from utilities import variables_interpolator as vi
+from utilities import census_interpolated_files_consolidator as cen_int_cons
 
 
 # replicate fixed columns into required number of times depending on year
@@ -15,23 +16,22 @@ fcr.replicate_fixed_cols(df = pd.read_csv('/Users/salma/Studies/Research/Crimina
                          op_fl_path='/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census',
                          dt_type='Census')
 
-'''
 
 # replicate year set from 90-15 unique ORI number of times
-yr.generate_years_90_15(df = pd.read_csv(
+yr.replicate_years_90_15(df = pd.read_csv(
                                 '/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census/Census_Fixed_Cols_Replicated.csv'),
                          repl_times = 14542,
                          dt_type = 'Census',
                          op_fl_path='/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census')
 
-'''
 
 # generate years between existing decennial years
+'''
 yg.generate_years(df=pd.read_csv(
                                 '/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census/Census_Fixed_Cols_Replicated.csv'),
                          dt_type = 'Census',
                          op_fl_path='/Users/salma/Studies/Research/Criminal_Justice/us_crime_analytics/data/census')
-
+'''
 
 # interpolate variable values in between decennial years
 vi.interpolate_vars(df=pd.read_csv('C:/Users/sshaik2/projects/criminal_justice/us-crime-analytics/data/economic/new_eco_cen/Census_90-15_Final_Sorted.csv'),
@@ -45,6 +45,7 @@ vi.interpolate_vars(df=pd.read_csv('C:/Users/sshaik2/projects/criminal_justice/u
 ############## TO-DO ####################
 do the below
 '''
-# replace_negative_values()
+cen_int_cons.replace_negative_values()
+cen_int_cons.write_final_intpltd_file()
 # write_final_intpltd_file()
 
