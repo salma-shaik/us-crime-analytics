dfe30dddcf5ce184d793d9a4061492c086b6923e
salma-shaik <salmashaik1611@gmail.com>
2019-09-05 16:01:15 -0400

    separated out incarc counts from data merger

--- utilities/incarceration_occupancy_counts_calculator.py
+++ utilities/incarceration_occupancy_counts_calculator.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 
-def calculate_incarc_counts():
-    final_main_race_counts_crime_totals['prison_occupancy_count'] = (final_main_race_counts_crime_totals['perc_felonies'] * final_main_race_counts_crime_totals['total_prison_pop']) / 100
-    final_main_race_counts_crime_totals['jail_occupancy_count'] = (final_main_race_counts_crime_totals['perc_misdemeanors'] * final_main_race_counts_crime_totals['jail_interp']) / 100
+def calculate_incarc_counts(df):
+    df['prison_occupancy_count'] = (df['perc_felonies'] * df['total_prison_pop']) / 100
+    df['jail_occupancy_count'] = (df['perc_misdemeanors'] * df['jail_interp']) / 100
+
+    return df
