0981fc8919866b4bb6733d47d966d75439ce410d
Salma Shaik <salmashaik1611@gmail.com>
2019-09-19 08:44:07 -0400

    to remove outliers from pos file

--- analysis/initial_core_file_retriever.py
+++ analysis/initial_core_file_retriever.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 # getting only the main columns for initial trend lines
 def get_top_level_vars(fnl_main):
+
     initial_core_df = fnl_main.loc[:, ['ORI', 'AGENCY', 'placename', 'Govt_level', 'place_fips', 'STATEFP', 'CNTY',
-                                       'YEAR', 'POP100','White_count', 'Black_count', 'Hispanic_count',
+                                       'YEAR', 'crime_year', 'POP100','White_count', 'Black_count', 'Hispanic_count',
                                        'Age1524_WhiteM','White_Males_All', 'Age1524_WhiteF', 'White_Females_All',
                                        'Age1524_BlackM','Black_Males_All', 'Age1524_BlackF','Black_Females_All',
                                        'Hispanic_Males_All','Age1524_HispanicM', 'Age1524_HispanicF',
@@ -11,24 +12,27 @@ def get_top_level_vars(fnl_main):
                                        'population', 'murder', 'rape', 'robbery', 'aggravated_assault',
                                        'simple_assault', 'burglary', 'larceny', 'auto_theft', 'violent_crime',
                                        'property_crime','total_main_crime','murder_rate', 'rape_rate',
-                                       'robbery_rate','aggravated_assault_rate','simple_assault_rate',
-                                       'burglary_rate', 'larceny_rate','auto_theft_rate','violent_crime_rate',
-                                       'property_crime_rate','total_main_crime_rate','violent_arrests',
-                                       'property_arrests', 'total_main_arrests', 'violent_arrests_rates','property_arrests_rates',
-                                       'total_main_arrests_rates', 'sale_drug_total_tot_arrests_rate',
-                                       'poss_drug_total_tot_arrests_rate', 'disorder_arrests_tot_index_rate',
+                                       'robbery_rate','aggravated_assault_rate','simple_assault_rate', 'property_arrests_rates',
+                                       'burglary_rate', 'larceny_rate','auto_theft_rate','violent_crime_rate','total_crime_rate',
+                                       'property_crime_rate','total_main_crime_rate','violent_arrests', 'drug_arrests_black',
+                                       'drug_arrests_white', 'property_arrests', 'total_main_arrests', 'violent_arrests_rates',
+                                       'total_main_arrests_rates', 'sale_drug_total_tot_arrests_rate', 'drug_arrests_black_rate',
+                                       'drug_arrests_white_rate','disorder_arrests_black_index_rate', 'disorder_arrests_white_index_rate',
+                                       'poss_drug_total_tot_arrests_rate', 'disorder_arrests_tot_index', 'disorder_arrests_black_index',
+                                       'disorder_arrests_tot_index_rate', 'disorder_arrests_white_index',
                                        'pci_total_pop','pci_white', 'pci_black', 'pci_hisp', 'emp_total',
                                        'emp_total_male','emp_total_female','emp_total_male_white',
                                        'emp_total_female_white','emp_total_male_black', 'emp_total_female_black',
                                        'emp_total_male_hisp','emp_total_female_hisp', 'emp_total_white', 'emp_total_black',
-                                       'emp_total_hisp', 'PRIMARY_LATITUDE', 'PRIMARY_LONGITUDE', 'total_officers',
+                                       'emp_total_hisp', 'total_officers',
                                        'total_officers_rate', 'perc_felonies', 'perc_misdemeanors','prison_occupancy_count',
-                                       'jail_occupancy_count', 'icspr_rep_code']]
+                                       'jail_occupancy_count', 'ncrp_incarc_rep_code']]
 
 
-    initial_core_df.to_csv('C:/Users/sshaik2/projects/criminal_justice/us-crime-analytics/data/analysis/initial_core_file.csv', index=False)
 
-    return initial_core_df
+    initial_core_df.to_csv('/Users/salma/Research/us-crime-analytics/data/analysis/initial_core_file.csv', index=False)
+
+    #return initial_core_df
 
 
 # Create weighted averages
@@ -54,4 +58,4 @@ def create_weighted_avgs(df, avg_name, weight_name):
 #     plt.show()
 
 
-get_top_level_vars(pd.read_csv('C:/Users/sshaik2/projects/criminal_justice/us-crime-analytics/data/analysis/final_main_ncrp_incarc_rep_code_enhanced.csv'))
+get_top_level_vars(pd.read_csv('/Users/salma/Research/us-crime-analytics/data/analysis/final_main_ncrp_incarc_rep_code_enhanced.csv'))
