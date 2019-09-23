92c55527667e54ea2cf129bdc06877ed5178b8fa
salma-shaik <salmashaik1611@gmail.com>
2019-09-23 17:30:52 -0400

    wip to identify outliers

---
+++ analysis/initial_core_files_outliers.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import seaborn as sns
+ini_core_counts_pos = pd.read_csv('C:/Users/sshaik2/projects/criminal_justice/us-crime-analytics/data/analysis/initial_core_counts_pos.csv')
+
