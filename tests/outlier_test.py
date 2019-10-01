0d1c461a548454ed7b3c362b390e785fa7bd779f
Salma Shaik <salmashaik1611@gmail.com>
2019-10-01 01:39:30 -0400

    added tests for outliers

--- tests/outlier_test.py
+++ tests/outlier_test.py
@@ -7,6 +7,8 @@ from numpy.random import randn
 from numpy import mean
 from numpy import std
 
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.width', 5000)
 # seed the random number generator
 seed(1)
 
@@ -17,20 +19,61 @@ test_df = pd.DataFrame({'a': [10,12,12,13,12,11,14,13,15,10,10,10,100,12,14],
 # summarize
 # print('mean: ', test_df.mean(),'\n', 'std: ', test_df.std())
 
-def drop_numerical_outliers(df, z_thresh=3):
-    # Constrains will contain `True` or `False` depending on if it is a value below the threshold.
-    # non_outliers = df.select_dtypes(include=[np.number]).apply(lambda x: np.abs(stats.zscore(x)) < z_thresh,
-    #                                                            result_type='reduce').all(axis=1)
-    # # Drop (inplace) values set to be rejected
-    # df.drop(df.index[~non_outliers], inplace=True)
-    # return df
 
-    outliers = df.select_dtypes(include=[np.number]).apply(lambda x: np.abs(stats.zscore(x)) > z_thresh, result_type='reduce').any(axis=1)
-    #df.drop(df.index[outliers], inplace=True)
+def get_outliers(df, z_thresh=3):
+    df_num = df.loc[:, 'POP100':]
 
-    return df.loc[df.index[outliers]]
+    # 3 z
+    # df_num_out_repl = df_num.mask(df_num.sub(df_num.mean()).div(df_num.std()).abs().gt(3)) --> worked
+    #df_num_out_repl = df.mask(df_num.sub(df_num.mean()).div(df_num.std()).abs().gt(3))
+
+    # if we don't take only numeric, then NaNs on char cells
+    df_num_out_repl = df.where(((df_num - df_num.mean()) / df_num.std()).abs() < 3, np.nan)
+
+    # 2 z
+
+    print(df_num_out_repl.head())
+
+    # df_num_out_repl_merged = df.loc[:, :'Govt_level'].merge(df_num_out_repl, left_index=True, right_index=True)
+    #
+    # return df_num_out_repl_merged
 
 
-non_outlier_test_df = drop_numerical_outliers(test_df)
+df = pd.read_csv('/Users/salma/Research/us-crime-analytics/data/pre_analysis/initial_core_counts_pop_1000_neg_rplcd.csv')
+outlier_test_df = get_outliers(df)
 
-print(non_outlier_test_df)
+#outlier_test_df.to_csv('/Users/salma/Research/us-crime-analytics/data/tests/outlier_test_df_3z1.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+#
+# print(outlier_test_df.shape[0])
+
+
+# df_num_out_repl = df_num.mask(df_num.sub(df_num.mean()).div(df_num.std()).abs().gt(2))
+
+# df_num_out_repl = df_num.mask(df_num.sub(df_num.mean()).div(df_num.std()).abs().gt(3))
+#
+# # mask() is the inverse boolean operation of where.
+# df.mask(df.sub(df.mean()).div(df.std()).abs().gt(2))
+#
+# df_num_out_repl = df_num.mask(df_num.sub(df_num.mean()).div(df_num.std()).abs().gt(2))
+#
+# # mod_df_out_repl_merged = df.loc[:, :'Govt_level'].merge(df_num_out_repl, left_index=True, right_index=True)
+# # print(mod_df_out_repl_merged.head())
+
+# df_num_out_repl = df_num.where(((df_num - df_num.mean())/df_num.std()).abs() < 3, np.nan)
+
+'''
+df_num_out_repl = df.mask(df.sub(df.mean()).div(df.std()).abs().gt(3))
+print(df_num_out_repl)
+return df_num_out_repl
+'''
+# return df.loc[df.index[outliers]]
--- tests/test.py
+++ tests/test.py
@@ -66,8 +66,109 @@ import pandas as pd
 # yr_str_dt = pd.to_datetime(yr_str)
 # print(yr_str_dt.year)
 
-df1 = pd.DataFrame({'a': [1, 1, 1, 2, 2]})
-df2 = pd.DataFrame({'a': [1, 2], 'b':['a_replicated', 'b_replicated']})
+# df1 = pd.DataFrame({'a': [1, 1, 1, 2, 2]})
+# df2 = pd.DataFrame({'a': [1, 2], 'b':['a_replicated', 'b_replicated']})
+#
+# df = df1.merge(df2, on='a')
+# print(df)
 
-df = df1.merge(df2, on='a')
-print(df)
+import numpy as np
+import operator
+
+raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
+        'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze'],
+        'age': [42, 52, 36, 24, 73],
+        'preTestScore': [-19, -999, -34534, 2, 1],
+        'postTestScore': [2, 2, -100, 2, -999]}
+df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
+
+# Accessing the private _get_numeric_data() of the dataframe. Private method, so changes reflected in the original df
+# int_df_num = int_df._get_numeric_data()
+# int_df_num[int_df_num < 0] = 0
+
+#df_num = df._get_numeric_data()
+
+#df_num[df_num < 0] = 0
+# r'[^-\d]', ""
+# df[df.postTestScore < 0] = np.nan
+
+#df[df.postTestScore < 0] = 'NaN'
+
+#df.replace('', np.nan, inplace=True)
+
+# Replace values where the condition is False.
+# data_frame = df.where(df > 0, np.nan)
+#
+#
+# df_num = df._get_numeric_data()
+# df_num[df_num < 0] = np.nan
+#
+# print(df)
+# print()
+# print(df.dtypes)
+# print()
+# print(df.preTestScore.mean())
+# print()
+# print(df.postTestScore.mean())
+
+
+#modDfObj = df.apply(lambda x: x + 10)
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.width', 5000)
+
+data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy', 'Roger', 'Ryan'],
+        'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze', 'Ben', 'Olsen'],
+        'age': [42, 52, 36, 24, 1007389, 797809, 3456788],
+        'preTestScore': [19, 999, 345348, 946578,23456, 2, 1],
+        'postTestScore': [2, 2, 100, 2, 99956, 34567, 2345678]}
+df_out_test = pd.DataFrame(data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
+
+#print(df_out_test, '\n')
+#mod_df = df_out_test.select_dtypes(include=[np.number]).apply(lambda x: ((x - x.mean())/x.std()).abs()>2, np.nan)
+
+#
+# def repl_outliers(ser):
+#
+#         if ((ser-ser.mean())/ser.std()) > 2:
+#                 return np.nan
+#         else:
+#                 return ser
+
+#print(df_out_test.loc[:, 'age':])
+df_num = df_out_test.loc[:, 'age':]
+
+df_num_out_repl = df_num.mask(df_num.sub(df_num.mean()).div(df_num.std()).abs().gt(2))
+
+print(df_num_out_repl)
+
+# df_num_out_repl = df_num.apply(repl_outliers)
+#
+# print(df_num_out_repl)
+
+# df_num_z = df_num.apply(lambda x: ((x - x.mean())/x.std()).abs())
+#
+# print(df_num_z, '\n')
+#
+# df_num = df_num.apply(lambda x: np.nan if ((((x - x.mean())/x.std()).abs()) > 2).any() else x)
+# print(df_num, '\n')
+#
+# df_num = np.where(fake_abalone2['Rings']<10, 'K', fake_abalone2['Sex'])
+
+#
+# # mod_df = df_out_test.iloc[:, 2:].apply(lambda x: np.nan if operator.gt((((x - x.mean())/x.std()).abs()), 2) else x)
+#
+# print(df_num, '\n')
+
+# df_out_test_merged = df_out_test.iloc[:,:2].merge(mod_df, left_index=True, right_index=True)
+# print(df_out_test_merged, '\n')
+
+#
+# df.loc[:,~df.columns.str.startswith('Test')]
+#
+#
+# df_out_test_merged = df_out_test_merged.loc[:,~df_out_test_merged.columns.str.endswith('_x')]
+# print(df_out_test_merged)
+#
+# df_out_test_merged.columns = df_out_test_merged.columns.str.rstrip('_y')
+#initial_core_counts_pos = initial_core_counts.merge(initial_core_counts_neg, how='left', indicator=True)
+# initial_core_counts_pos = initial_core_counts_pos[initial_core_counts_pos['_merge'] == 'left_only']
